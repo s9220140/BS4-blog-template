a5cc5a2e654b2cd31d185759f69012f477dc4493
goodloser <leaserman0906@gmail.com>
2022-06-14 10:58:10 +0800

    20220614

--- js/all.js
+++ js/all.js
@@ -1,10 +1,42 @@
-$(document).ready(function () {
-    $('.fa-thumbs-up').click(function (e) { 
-        e.preventDefault();
-        $(this).toggleClass('fas');
-    });
-    $('.fa-thumbs-down').click(function (e) { 
-        e.preventDefault();
-        $(this).toggleClass('fas');
-    }); 
+// 表單驗證
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+// go top
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 200) {
+    $(".go-top").fadeIn(200);
+  } else {
+    $(".go-top").fadeOut(200);
+  }
+});
+// Animate the scroll to top
+$(".go-top").click(function (e) {
+  e.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: 0,
+    },
+    300
+  );
 });
